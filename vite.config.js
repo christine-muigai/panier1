650e902491749f1ec76e44920ca6956d7ff4ee05
christine-muigai <git@github.com>
2025-04-26 16:28:47 +0300

    Auto commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import WindiCSS from 'vite-plugin-windicss'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
+  plugins: [react(), WindiCSS()],
 })
