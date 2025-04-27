892e16160dd295190691eaf20d837e113d03a68b
christine-muigai <git@github.com>
2025-04-27 18:17:27 +0300

    Auto commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+
+      'firebase.js': path.resolve(__dirname, 'firebase.js')
+    }
+  },
+  define: {
+    'process.env': process.env
+  }
 })
